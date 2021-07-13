222605d908d22efc31b01bd95c471bef457746d0
Ilana Mercer <ilana.rose.mercer@gmail.com>
2021-07-13 09:35:26 -0400

    creates an Add Choice section in admin form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
 
 admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
